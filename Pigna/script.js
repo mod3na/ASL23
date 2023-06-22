37a4d508e43a1c86d6f70990ab9b80e80b04542f
mod3na <enr.modena@gmail.com>
2023-06-22 17:24:47 +0200

    scroll

--- Pigna/script.js
+++ Pigna/script.js
@@ -1,22 +1,22 @@
 
-let pignaClass;
-let provaPigna
+//(window.scrollY + document.querySelector('#Livello_1').getBoundingClientRect().top)
+
+
+var pignaClass;
 window.onload = function(){
     pignaClass = document.querySelector('#Livello_1 .cls-1');
-    provaPigna = document.querySelector('Livello1 .cls2 1');
+
 }
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
     console.log(scroll)
 
-    if(scroll >= (window.scrollY + document.querySelector('#Livello_1').getBoundingClientRect().top)){
-        pignaClass.style.fill = 'red' 
-    }else if(scroll < (window.scrollY + document.querySelector('#Livello_1').getBoundingClientRect().top)){
+    if(scroll >= 200){
+        pignaClass.style.fill = 'red'
+        
+ 
+    }else if(scroll < 200){
         pignaClass.style.fill = '#93c01f'
     }
-    console.log(pignaClass.style.fill)
     
-    provaPigna.style.transform = "translateX(30px)";
-
-
 });
