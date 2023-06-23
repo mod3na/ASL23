7dbbfd530bbdefbfaa4da26760d602f8b1817228
mod3na <enr.modena@gmail.com>
2023-06-23 09:54:29 +0200

    sistema colore logo funzionante

--- Pigna/script.js
+++ Pigna/script.js
@@ -1,11 +1,16 @@
 
-//(window.scrollY + document.querySelector('#Livello_1').getBoundingClientRect().top)
+//
 
-let clas1,ClassGrafica;
+let clas1,ClassGrafica,ClassStampa,ClassInsegne,ClassPromo,ClassAllestimenti;
 let headerClass;
 
 window.onload = function(){
     ClassGrafica = document.getElementsByClassName('grafica');
+    ClassStampa = document.getElementsByClassName('stampa');
+    ClassInsegne = document.getElementsByClassName('insegne');
+    ClassPromo = document.getElementsByClassName('promozionale');
+    ClassAllestimenti = document.getElementsByClassName('allestimenti');
+    
     clas1 = document.getElementsByClassName('cls-1');
     headerClass = document.getElementsByClassName("header")[0]
 
@@ -15,15 +20,67 @@ window.addEventListener("scroll", (event) => {
     console.log(scroll)
     console.log(ClassGrafica)
 
-    if(scroll >= 200){
+    if(scroll >= (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top)){
         for(let i = 0; i<clas1.length;i++){
             clas1[i].style.setProperty("fill","transparent");
         }
         for(let i = 0; i<ClassGrafica.length; i++){
             ClassGrafica[i].style.setProperty("fill","#D89BED","important");
         }
-        
- 
+    }
+
+    if(scroll >= (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top)){
+        for(let i = 0; i<clas1.length;i++){
+            clas1[i].style.setProperty("fill","transparent");
+        }
+        for(let i = 0; i<ClassGrafica.length; i++){
+            ClassGrafica[i].style.setProperty("fill","#D89BED","important");
+        }
+    }
+
+    if(scroll >= (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top)){
+        for(let i = 0; i<clas1.length;i++){
+            clas1[i].style.setProperty("fill","transparent");
+        }
+        for(let i = 0; i<ClassGrafica.length; i++){
+            ClassGrafica[i].style.setProperty("fill","#D89BED","important");
+        }
+    }
+
+    if(scroll >= (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top)){
+        for(let i = 0; i<clas1.length;i++){
+            clas1[i].style.setProperty("fill","transparent");
+        }
+        for(let i = 0; i<ClassGrafica.length; i++){
+            ClassGrafica[i].style.setProperty("fill","#D89BED","important");
+        }
+    }
+    
+    if(scroll >= (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top)){
+        for(let i = 0; i<clas1.length;i++){
+            clas1[i].style.setProperty("fill","transparent");
+        }
+        for(let i = 0; i<ClassGrafica.length; i++){
+            ClassGrafica[i].style.setProperty("fill","#D89BED","important");
+        }
+    }
+
+    if(scroll >= (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top)){
+        for(let i = 0; i<clas1.length;i++){
+            clas1[i].style.setProperty("fill","transparent");
+        }
+        for(let i = 0; i<ClassGrafica.length; i++){
+            ClassGrafica[i].style.setProperty("fill","#D89BED","important");
+        }
+    }
+
+    if(scroll >= (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top)){
+        for(let i = 0; i<clas1.length;i++){
+            clas1[i].style.setProperty("fill","transparent");
+        }
+        for(let i = 0; i<ClassGrafica.length; i++){
+            ClassGrafica[i].style.setProperty("fill","#D89BED","important");
+        }
     }
     
 });
