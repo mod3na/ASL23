cd46f3100c2d4aadc954e9fac4ad6d663a009439
mod3na <enr.modena@gmail.com>
2023-06-23 10:30:22 +0200

    change colore logo funzionante

--- Pigna/script.js
+++ Pigna/script.js
@@ -20,7 +20,14 @@ window.addEventListener("scroll", (event) => {
     console.log(scroll)
     console.log(ClassGrafica)
 
-    if(scroll >= (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top)){
+    if(scroll >= (window.scrollY + document.querySelector('#studio').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#studio').getBoundingClientRect().bottom)){
+
+        for(let i = 0; i<clas1.length; i++){
+            clas1[i].style.setProperty("fill","#9BBC06","important");
+        }
+    }
+
+    if(scroll >= (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().bottom)){
         for(let i = 0; i<clas1.length;i++){
             clas1[i].style.setProperty("fill","transparent");
         }
@@ -29,57 +36,39 @@ window.addEventListener("scroll", (event) => {
         }
     }
 
-    if(scroll >= (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top)){
+    if(scroll >= (window.scrollY + document.querySelector('#stampa').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#stampa').getBoundingClientRect().bottom)){
         for(let i = 0; i<clas1.length;i++){
             clas1[i].style.setProperty("fill","transparent");
         }
-        for(let i = 0; i<ClassGrafica.length; i++){
-            ClassGrafica[i].style.setProperty("fill","#D89BED","important");
+        for(let i = 0; i<ClassStampa.length; i++){
+            ClassStampa[i].style.setProperty("fill","#44BBA4","important");
         }
     }
 
-    if(scroll >= (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top)){
+    if(scroll >= (window.scrollY + document.querySelector('#insegne').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#insegne').getBoundingClientRect().bottom)){
         for(let i = 0; i<clas1.length;i++){
             clas1[i].style.setProperty("fill","transparent");
         }
-        for(let i = 0; i<ClassGrafica.length; i++){
-            ClassGrafica[i].style.setProperty("fill","#D89BED","important");
-        }
-    }
-
-    if(scroll >= (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top)){
-        for(let i = 0; i<clas1.length;i++){
-            clas1[i].style.setProperty("fill","transparent");
-        }
-        for(let i = 0; i<ClassGrafica.length; i++){
-            ClassGrafica[i].style.setProperty("fill","#D89BED","important");
+        for(let i = 0; i<ClassInsegne.length; i++){
+            ClassInsegne[i].style.setProperty("fill","#FF6F59","important");
         }
     }
     
-    if(scroll >= (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top)){
+    if(scroll >= (window.scrollY + document.querySelector('#promozionale').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#promozionale').getBoundingClientRect().bottom)){
         for(let i = 0; i<clas1.length;i++){
             clas1[i].style.setProperty("fill","transparent");
         }
-        for(let i = 0; i<ClassGrafica.length; i++){
-            ClassGrafica[i].style.setProperty("fill","#D89BED","important");
+        for(let i = 0; i<ClassPromo.length; i++){
+            ClassPromo[i].style.setProperty("fill","#0B4F6C","important");
         }
     }
 
-    if(scroll >= (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top)){
+    if(scroll >= (window.scrollY + document.querySelector('#allestimenti').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#allestimenti').getBoundingClientRect().bottom)){
         for(let i = 0; i<clas1.length;i++){
             clas1[i].style.setProperty("fill","transparent");
         }
-        for(let i = 0; i<ClassGrafica.length; i++){
-            ClassGrafica[i].style.setProperty("fill","#D89BED","important");
-        }
-    }
-
-    if(scroll >= (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top) && scroll < (window.scrollY + document.querySelector('#grafica').getBoundingClientRect().top)){
-        for(let i = 0; i<clas1.length;i++){
-            clas1[i].style.setProperty("fill","transparent");
-        }
-        for(let i = 0; i<ClassGrafica.length; i++){
-            ClassGrafica[i].style.setProperty("fill","#D89BED","important");
+        for(let i = 0; i<ClassAllestimenti.length; i++){
+            ClassAllestimenti[i].style.setProperty("fill","#FFD400","important");
         }
     }
    
